85fea202b14af1119073508e99f31a20ef7caeb2
abdrasaq14 <abdrasaq14@gmail.com>
2024-07-28 10:33:07 +0100

    course page in progress

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title.form.tsx
@@ -0,0 +1,55 @@
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Pencil } from "lucide-react";
+const formSchema = z.object({
+  title: z.string().min(3, { message: "Title is too short" })
+});
+
+interface TitleFormProps {
+  courseId: string;
+  initialData: {
+    title: string;
+  };
+}
+export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course title
+        <Button variant="ghost">
+          <Pencil className="h-4 w-4 mr-2" />
+          Edit title
+        </Button>
+      </div>
+    </div>
+  );
+};
